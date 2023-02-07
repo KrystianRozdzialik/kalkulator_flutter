bbe3daa8ad4080b88e64249d0be8ba0c60224471
Krystian <kryroz21@gmail.com>
2023-02-07 12:56:19 +0100

    final project

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Container(
 
       child: ElevatedButton(onPressed: (){
-        //code
+        calculation(btntxt);
       },
 
         child: Text(btntxt,
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         style: ElevatedButton.styleFrom(
           backgroundColor: btncolor,
-                                                  // shadowColor: Color(0xFFFFF),
+          // shadowColor: Color(0xFFFFF),
           //shape: CircleBorder(),
         ),
       ),
@@ -71,12 +71,12 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [ Padding(padding: EdgeInsets.all(30),
-                child: Text('0',
+                child: Text(text,
                   textAlign: TextAlign.left,
                   style: TextStyle(color: Colors.black,
                       fontSize: 100),
-                  ),
-                )
+                ),
+              )
               ],
             ),
             SizedBox(height: 50,),
@@ -217,5 +217,119 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+  dynamic text ='0';
+  double numOne = 0;
+  double numTwo = 0;
+
+  dynamic result = '';
+  dynamic finalResult = '';
+  dynamic opr = '';
+  dynamic preOpr = '';
+  void calculation(btnText) {
+
+
+    if(btnText  == 'AC') {
+      text ='0';
+      numOne = 0;
+      numTwo = 0;
+      result = '';
+      finalResult = '0';
+      opr = '';
+      preOpr = '';
+
+    } else if( opr == '=' && btnText == '=') {
+
+      if(preOpr == '+') {
+        finalResult = add();
+      } else if( preOpr == '-') {
+        finalResult = sub();
+      } else if( preOpr == 'x') {
+        finalResult = mul();
+      } else if( preOpr == '/') {
+        finalResult = div();
+      }
+
+    } else if(btnText == '+' || btnText == '-' || btnText == 'x' || btnText == '/' || btnText == '=') {
+
+      if(numOne == 0) {
+        numOne = double.parse(result);
+      } else {
+        numTwo = double.parse(result);
+      }
+
+      if(opr == '+') {
+        finalResult = add();
+      } else if( opr == '-') {
+        finalResult = sub();
+      } else if( opr == 'x') {
+        finalResult = mul();
+      } else if( opr == '/') {
+        finalResult = div();
+      }
+      preOpr = opr;
+      opr = btnText;
+      result = '';
+    }
+    else if(btnText == '%') {
+      result = numOne / 100;
+      finalResult = doesContainDecimal(result);
+    } else if(btnText == '.') {
+      if(!result.toString().contains('.')) {
+        result = result.toString()+'.';
+      }
+      finalResult = result;
+    }
+
+    else if(btnText == '+/-') {
+      result.toString().startsWith('-') ? result = result.toString().substring(1): result = '-'+result.toString();
+      finalResult = result;
+
+    }
+
+    else {
+      result = result + btnText;
+      finalResult = result;
+    }
+
+
+    setState(() {
+      text = finalResult;
+    });
+
+  }
+
+
+  String add() {
+    result = (numOne + numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String sub() {
+    result = (numOne - numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+  String mul() {
+    result = (numOne * numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+  String div() {
+    result = (numOne / numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+
+  String doesContainDecimal(dynamic result) {
+
+    if(result.toString().contains('.')) {
+      List<String> splitDecimal = result.toString().split('.');
+      if(!(int.parse(splitDecimal[1]) > 0))
+        return result = splitDecimal[0].toString();
+    }
+    return result;
   }
 }
